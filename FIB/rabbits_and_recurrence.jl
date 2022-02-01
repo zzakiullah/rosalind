0a83532dd899eccb065ef0a1e4b9a70da2e62fdf
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2022-02-01 01:56:27 -0500

    Finish RNA problem

---
+++ FIB/rabbits_and_recurrence.jl
@@ -0,0 +1,6 @@
+# ID:       FIB
+# Title:    Rabbits and Recurrence Relations
+
+function rabbits_and_recurrence()
+    print("Rabbits and recurrence")
+end
--- RNA/transcribing_dna_into_rna.r
+++ RNA/transcribing_dna_into_rna.r
@@ -1,8 +1,7 @@
 # ID:     RNA
 # Title:  Transcribing DNA into RNA
 
-transcribing_dna_into_rna <- function(filename) {
-  dna <- ""
-  rna <- str_replace_all(dna, "T", "U")
-  return(rna)
+transcribing_dna_into_rna <- function(dna) {
+  rna <- gsub("T", "U", dna)
+  print(rna)
 }
